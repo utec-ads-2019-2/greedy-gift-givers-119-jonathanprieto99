685a2e8566793e8f56c944f57df9ab1bbb2f25e1
Jonathan Prieto <jonathan.prieto@me.com>
2019-09-24 14:09:37 -0500

    First upload

---
+++ cmake-build-debug/CMakeFiles/greedy-gift-givers-119-jonathanprieto99.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/greedy-gift-givers-119-jonathanprieto99.dir/main.cpp.o"
+  "greedy-gift-givers-119-jonathanprieto99.pdb"
+  "greedy-gift-givers-119-jonathanprieto99"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/greedy-gift-givers-119-jonathanprieto99.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/greedy-gift-givers-119-jonathanprieto99.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for greedy-gift-givers-119-jonathanprieto99.
+# This may be replaced when dependencies are built.
