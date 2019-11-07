d4d80351e268fc3acf977981d0add80bf1ec93a7
Jonathan Prieto <jonathan.prieto@me.com>
2019-11-07 00:14:14 -0500

    Se agrego string.h

--- cmake-build-debug/CMakeFiles/greedy-gift-givers-119-jonathanprieto99.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/greedy-gift-givers-119-jonathanprieto99.dir/cmake_clean.cmake
@@ -1,7 +1,7 @@
 file(REMOVE_RECURSE
   "CMakeFiles/greedy-gift-givers-119-jonathanprieto99.dir/main.cpp.o"
-  "greedy-gift-givers-119-jonathanprieto99.pdb"
   "greedy-gift-givers-119-jonathanprieto99"
+  "greedy-gift-givers-119-jonathanprieto99.pdb"
 )
 
 # Per-language clean rules from dependency scanning.
